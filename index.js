5c79403dc22739b57b5f972399342182831c99ce
displayportdog <umair.dingmar05@gmail.com>
2021-12-30 14:57:52 +0000

    refactored the main thingy, you know. a lot shorter now.

--- index.js
+++ index.js
@@ -39,135 +39,24 @@ module.exports = async function getTitle(pageURL, verbose = false) {
 
         let holdTemp = [];
 
-        $('div[id="main"]')
-            .find('p[class="caution"]').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text()));
-            }
-
-        )
-
-        if (holdTemp[0]){
-            if (holdTemp.toString().includes('adult')){
+            if (getElementsBy($, 'div[id="main"]', 'p[class="caution"]', verbose).toString().includes('adult')){
                 ficInfo.sfw	= false;
                 if (verbose === true){ console.log(`Fic is NSFW - limited data available`) }
             }
-        }
-        if (verbose === true){ console.log(holdTemp) }
-        holdTemp = [];
 
+        ficInfo.authors = getElementsBy($, 'div[class="preface group"]', 'h3[class="byline heading"] > a[rel="author"]', verbose);
+        ficInfo.title = getElementsBy($, 'div[class="preface group"]', 'h2[class="title heading"]', verbose);
+        ficInfo.category = getElementsBy($, 'dd[class="category tags"]', 'ul[class="commas"] > li > a[class="tag"]', verbose);
+        ficInfo.tags = getElementsBy($, 'dd[class="freeform tags"]', 'ul[class="commas"] > li > a[class="tag"]', verbose);
+        ficInfo.summary = getElementsBy($, 'div[class="summary module"]', 'blockquote[class="userstuff"] > p', verbose)[0];
+        ficInfo.rating = getElementsBy($, 'dd[class="rating tags"]', 'ul[class="commas"] > li > a[class="tag"]', verbose)[0];
 
-        $('div[class="preface group"]')
-            .find('h3[class="byline heading"] > a[rel="author"]').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-
-            }
-
-        )
-
-        if (verbose === true){ console.log(holdTemp) }
-        ficInfo.authors = holdTemp;
-        holdTemp = [];
-
-        $('div[class="preface group"]')
-            .find('h2[class="title heading"]').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        if (verbose === true){ console.log(holdTemp) }
-        ficInfo.title = holdTemp[0];
-        holdTemp = [];
-
-        $('dd[class="category tags"]')
-            .find('ul[class="commas"] > li > a[class="tag"]').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        if (verbose === true){ console.log(holdTemp) }
-        ficInfo.category = holdTemp;
-        holdTemp = [];
-
-        $('dd[class="freeform tags"]')
-            .find('ul[class="commas"] > li > a[class="tag"]').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        if (verbose === true){ console.log(holdTemp) }
-        ficInfo.tags = holdTemp;
-        holdTemp = [];
-
-        $('div[class="summary module"]')
-            .find('blockquote[class="userstuff"] > p').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        if (verbose === true){ console.log(holdTemp) }
-        ficInfo.summary = holdTemp[0];
-        holdTemp = [];
-
-        $('dd[class="rating tags"]')
-            .find('ul[class="commas"] > li > a[class="tag"]').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        if (verbose === true){ console.log(holdTemp) }
-        ficInfo.rating = holdTemp[0];
+        //AUTHORPAGES
         holdTemp = [];
 
         $('div[class="preface group"]')
             .find('h3[class="byline heading"] > a[rel="author"]').each(
-            function( i ) {
+            function(  ) {
                 holdTemp.push($(this).attr('href'));
 
 
@@ -183,285 +72,34 @@ module.exports = async function getTitle(pageURL, verbose = false) {
         ficInfo.authorPages = holdTemp;
         holdTemp = [];
 
-        $('dl[class="stats"]')
-            .find('dd[class="chapters"]').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        if (verbose === true){ console.log(holdTemp) }
-        ficInfo.chapters = holdTemp[0];
-        holdTemp = [];
-
-        $('dl[class="stats"]')
-            .find('dd[class="published"]').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        if (verbose === true){ console.log(holdTemp) }
-        ficInfo.published = holdTemp[0];
-        holdTemp = [];
-
-        $('dl[class="stats"]')
-            .find('dd[class="words"]').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        if (verbose === true){ console.log(holdTemp) }
-        ficInfo.wordCount = parseInt(holdTemp[0]);
-        holdTemp = [];
-
-        $('dl[class="stats"]')
-            .find('dd[class="comments"]').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        if (verbose === true){ console.log(holdTemp) }
-        ficInfo.commentCount = parseInt(holdTemp[0]);
-        holdTemp = [];
-
-        $('dl[class="stats"]')
-            .find('dd[class="kudos"]').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        if (verbose === true){ console.log(holdTemp) }
-        ficInfo.kudosCount = parseInt(holdTemp[0]);
-        holdTemp = [];
-
-        $('dl[class="stats"]')
-            .find('dd[class="bookmarks"]').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        if (verbose === true){ console.log(holdTemp) }
-        ficInfo.bookmarkCount = parseInt(holdTemp[0]);
-        holdTemp = [];
-
-        $('dl[class="stats"]')
-            .find('dd[class="status"]').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        if (verbose === true){ console.log(holdTemp) }
-        ficInfo.updated = holdTemp[0];
-        holdTemp = [];
-
-        $('dl[class="stats"]')
-            .find('dd[class="hits"]').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        if (verbose === true){ console.log(holdTemp) }
-        ficInfo.hitCount = parseInt(holdTemp[0]);
-        holdTemp = [];
-
-        $('dl[class="work meta group"]')
-            .find('dd[class="language"]').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        if (verbose === true){ console.log(holdTemp) }
-        ficInfo.language = holdTemp[0];
-        holdTemp = [];
-
-        $('dd[class="relationship tags"]')
-            .find('ul > li > a').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        if (verbose === true){ console.log(holdTemp) }
-        ficInfo.pairings = holdTemp;
-        holdTemp = [];
-
-        $('dd[class="warning tags"]')
-            .find('ul > li > a').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        ficInfo.warnings = holdTemp;
-        if (verbose === true){ console.log(holdTemp) }
-        holdTemp = [];
-
-        $('dd[class="fandom tags"]')
-            .find('ul > li > a').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-            }
-
-        )
-
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        ficInfo.fandoms = holdTemp;
-        if (verbose === true){ console.log(holdTemp) }
-        holdTemp = [];
-
-        $('dd[class="collections"]')
-            .find('a').each(
-            function( i ) {
-                holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-            }
-
-        )
-        holdTemp = holdTemp.map(element => {
-            return element.trim();
-        });
-
-        ficInfo.collections = holdTemp;
-
-        if (verbose === true){ console.log(holdTemp) }
-        holdTemp = [];
+        //DONT MESS WITH THIS [YET ;)]
+
+        ficInfo.chapters = getElementsBy($, 'dl[class="stats"]', 'dd[class="chapters"]', verbose)[0];
+        ficInfo.published = getElementsBy($, 'dl[class="stats"]', 'dd[class="published"]', verbose)[0];
+        ficInfo.wordCount = parseInt(getElementsBy($, 'dl[class="stats"]', 'dd[class="words"]', verbose)[0]);
+        ficInfo.commentCount = parseInt(getElementsBy($, 'dl[class="stats"]', 'dd[class="comments"]', verbose)[0]);
+        ficInfo.kudosCount = parseInt(getElementsBy($, 'dl[class="stats"]', 'dd[class="kudos"]', verbose)[0]);
+        ficInfo.bookmarkCount = parseInt(getElementsBy($, 'dl[class="stats"]', 'dd[class="bookmarks"]', verbose)[0]);
+        ficInfo.updated = getElementsBy($, 'dl[class="stats"]', 'dd[class="status"]', verbose)[0];
+        ficInfo.hitCount = parseInt(getElementsBy($, 'dl[class="stats"]', 'dd[class="hits"]', verbose)[0]);
+        ficInfo.language = getElementsBy($, 'dl[class="work meta group"]', 'dd[class="language"]', verbose)[0];
+        ficInfo.pairings = getElementsBy($, 'dd[class="relationship tags"]', 'ul > li > a', verbose);
+        ficInfo.warnings = getElementsBy($, 'dd[class="warning tags"]', 'ul > li > a', verbose);
+        ficInfo.fandoms = getElementsBy($, 'dd[class="fandom tags"]', 'ul > li > a', verbose);
+        ficInfo.collections = getElementsBy($, 'dd[class="collections"]', 'a', verbose);
 
         if (ficInfo.sfw === false)
         {
 
-            $('h4[class="heading"]')
-                .find('a:not(a[rel="author"])').each(
-                function( i ) {
-                    holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
+            ficInfo.title = getElementsBy($, 'h4[class="heading"]', 'a:not(a[rel="author"])', verbose)[0];
+            ficInfo.author = getElementsBy($, 'h4[class="heading"]', 'a[rel="author"]', verbose);
+            ficInfo.fandoms = getElementsBy($, 'h5[class="fandoms heading"]', 'a[class="tag"]', verbose);
 
-                }
-
-            )
-
-            holdTemp = holdTemp.map(element => {
-                return element.trim();
-            });
-
-            ficInfo.title = holdTemp[0];
-            if (verbose === true){ console.log(holdTemp) }
-            holdTemp = [];
+            //AUTHORPAGES
 
             $('h4[class="heading"]')
                 .find('a[rel="author"]').each(
-                function( i ) {
-                    holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-                }
-
-            )
-
-            holdTemp = holdTemp.map(element => {
-                return element.trim();
-            });
-
-            ficInfo.author = holdTemp;
-            if (verbose === true){ console.log(holdTemp) }
-            holdTemp = [];
-
-            $('h5[class="fandoms heading"]')
-                .find('a[class="tag"]').each(
-                function( i ) {
-                    holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
-
-                }
-
-            )
-
-            holdTemp = holdTemp.map(element => {
-                return element.trim();
-            });
-
-            ficInfo.fandoms = holdTemp;
-            if (verbose === true){ console.log(holdTemp) }
-            holdTemp = [];
-
-
-            $('h4[class="heading"]')
-                .find('a[rel="author"]').each(
-                function( i ) {
+                function(  ) {
                     holdTemp.push($(this).attr('href'));
                 }
 
@@ -475,6 +113,8 @@ module.exports = async function getTitle(pageURL, verbose = false) {
             if (verbose === true){ console.log(holdTemp) }
             holdTemp = [];
 
+            //DO NOT MESS WITH THIS
+
             ficInfo.summary = "[Summaries for explicit works are hidden.]"
 
 
@@ -482,7 +122,7 @@ module.exports = async function getTitle(pageURL, verbose = false) {
 
 
 
-    }).catch(error => {
+    }).catch(err => {
         if (verbose === false){ throw `An error occured while trying to access that page`; } else { throw err; }
     })
 
@@ -490,3 +130,22 @@ module.exports = async function getTitle(pageURL, verbose = false) {
     return ficInfo;
 };
 
+function getElementsBy($, parent, element, verbose){
+    let holdTemp = [];
+
+    $(parent)
+        .find(element).each(
+        function(  ) {
+            holdTemp.push($( this ).toArray().map(el => $(el).text())[0]);
+
+        }
+
+    )
+
+    holdTemp = holdTemp.map(element => {
+        return element.trim();
+    });
+
+    if (verbose === true){ console.log(holdTemp) }
+    return holdTemp;
+}
